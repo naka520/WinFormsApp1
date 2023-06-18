8be2f5bdfc2a7683d3424f87bf40288ee16f96a4
naka520 <yashipipi0520@gmail.com>
2023-06-19 00:23:08 +0900

    文字列表示 親要素子要素の理解

--- WinFormsApp1/Program.cs
+++ WinFormsApp1/Program.cs
@@ -11,16 +11,23 @@ namespace WinFormsApp1
             Form fm;
             fm = new Form();
             fm.Text = "Hello";
-           
-
-
-            PictureBox[] pictureBox = new PictureBox[5];
-            for (int i = 0; i < pictureBox.Length; i++) {
-                pictureBox[i] = new PictureBox();
-                pictureBox[i].Image = Image.FromFile("c:\\car.bmp");
-                pictureBox[i].Top = i*pictureBox[i].Height;
-                pictureBox[i].Parent = fm;
+            fm.Width = 250; fm.Height = 100;
+            Label lb = new Label();
+            lb.Width = fm.Width; lb.Height=fm.Height;
+            string[] str = new string [3] { "‰”•M", "Á‚µƒSƒ€", "ƒyƒ“" }; 
+            foreach (string str2 in str) {
+             lb.Text += str2+"\n";
             }
+
+            // PictureBox[] pictureBox = new PictureBox[5];
+            //for (int i = 0; i < pictureBox.Length; i++) {
+            // pictureBox[i] = new PictureBox();
+            //pictureBox[i].Image = Image.FromFile("c:\\car.bmp");
+            //pictureBox[i].Top = i*pictureBox[i].Height;
+            // pictureBox[i].Parent = fm;
+            //  }
+
+            lb.Parent = fm;
             Application.Run(fm);
 
 
